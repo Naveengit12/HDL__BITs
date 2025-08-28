7baa239f3f9a9ce944c52d3ea58064257330f026
Naveengit12 <jikkilinaveen@gmail.com>
2025-08-28 17:22:32 +0530

    Create rotate100.v

---
+++ Sequential logic/rotate100.v
@@ -0,0 +1,16 @@
+module top_module(
+    input clk,
+    input load,
+    input [1:0] ena,
+    input [99:0] data,
+    output reg [99:0] q); 
+    
+    always @(posedge clk) begin
+		if (load)		// Load
+			q <= data;
+		else if (ena == 2'h1)	// Rotate right
+			q <= {q[0], q[99:1]};
+		else if (ena == 2'h2)	// Rotate left
+			q <= {q[98:0], q[99]};
+	end
+endmodule
